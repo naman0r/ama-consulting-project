ed1cd03bef64104f6fbcc597136536f15734dc52
IoannaDamianov <damianov.io@northeastern.edu>
2025-03-13 14:58:24 -0400

    Added buttons and styling to navbar and footer, added logo, no functionality yet - Ioanna

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "../styles/TopNav.css";
 
 const TopNav = () => {
-  return <div>TopNav</div>;
-};
+  return (
+  <nav className= "nav_bar">
+    <button className="nav_button">Home</button>
+    <button className="nav_button" >New Member</button>
+    <button className="nav_button">Login</button>
+    <button className="nav_button">Profile
+      <ion-icon name="person-circle-outline"></ion-icon>
+    </button>
+    <button className="nav_button">Settings</button>
+    </nav>
+  );
+}
 
 export default TopNav;
