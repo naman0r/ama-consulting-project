a38fc5415e206df2091d31b96587cb62b97e20de
IoannaDamianov <damianov.io@northeastern.edu>
2025-03-18 15:10:03 -0400

    restructured header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/TopNav.css";
-import logo from '../assets/logo-png.png'; 
+import logo from '../assets/logo-new.png'; 
 
 const Header = () => {
     return (
             <header>
             <img src={logo} alt="Logo"></img>
+            Ask Me Anything
             </header>
     );
 };
