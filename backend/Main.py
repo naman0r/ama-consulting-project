cf9f5d2b2215b503e0c5bc2d80e406830953612d
madhavnair007 <mdnair230@gmail.com>
2025-03-19 18:52:42 -0400

    wrote /test endpoint

--- backend/Main.py
+++ backend/Main.py
@@ -1,8 +1,18 @@
 # main backend entrypoint
 
 from flask import Flask, jsonify
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/', methods=["GET"])
 def hello_world():
@@ -11,7 +21,20 @@ def hello_world():
 
 @app.route('/test', methods=["GET"])
 def test_supabase(): 
-    return jsonify({"message": "to implement by madhav"})
+    try:
+        # read the elements in the test table
+        response = supabase.table("test").select("*").limit(5).execute()
+        
+        # if we get a response, Supabase is connected
+        if response.data:
+            return jsonify({"message": "Supabase connected successfully!"}, response.data), 200
+        else:
+            return jsonify({"error": "Supabase connected, but query returned no data"}), 500
+
+    except Exception as e:
+        # if there's an error, return failure
+        return jsonify({"error": str(e)}), 500
+   
     # can only reqest GET from this endpoint. 
 
 if __name__ == '__main__':
