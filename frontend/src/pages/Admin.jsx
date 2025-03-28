eca926dd479e3633aff0c45be3d899b77f23e794
Naman Rusia <naman.rusia14@gmail.com>
2025-03-28 16:22:51 -0400

    naman branch up to date

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -7,23 +7,19 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Admin() {
-  
   return (
     <>
       <Header />
       <TopNav />
-      <p className="p3">
-        Admin Dashboard
-      </p>
-      <button className="select_btn">
-        Select AMA!</button>
+      <p className="p3">Admin Dashboard</p>
+      <button className="select_btn">Select AMA!</button>
       <div className="admin_page">
-      <div className="ama_history">
-      <p className="p1">
-        feed with the past AMAs, including name, fun facts, and blurb,  will show here
-      </p>
-      </div>
-      
+        <div className="ama_history">
+          <p className="p1">
+            feed with the past AMAs, including name, fun facts, and blurb, will
+            show here
+          </p>
+        </div>
       </div>
       <Footer />
     </>
