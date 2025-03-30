5365eac60f2b6322861ce13b1ac7f2008df68dc1
kdani33 <dani.k@northeastern.edu>
2025-03-30 13:09:22 -0400

    select member page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "../styles/TopNav.css";
-import logo from "../assets/blue-logo.png";
-import tamid from "../assets/TAMID-Logo-transparent.png";
+import logo from '../assets/blue-logo.png'; 
+import tamid from '../assets/TAMID-Logo-transparent.png'; 
 
 const Header = () => {
-  return (
-    <header>
-      <img className="ama-logo" src={logo} alt="Logo"></img>
-      Ask Me Anything
-      <img className="tamid-logo" src={tamid} alt="Tamid"></img>
-    </header>
-  );
+    return (
+            <header>
+<<<<<<< HEAD
+            <img src={logo} alt="Logo"></img>
+            Ask Me Anything Automator!
+=======
+            <img 
+            className="ama-logo"
+            src={logo} alt="Logo">
+            </img>
+            Ask Me Anything
+            <img 
+            className="tamid-logo"
+            src={tamid} alt="Tamid"></img>
+>>>>>>> 892046815c845a91b1d5635858581ae215748aaa
+            </header>
+    );
 };
 
 export default Header;
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,22 +5,19 @@ import "../styles/Admin.css";
 import TopNav from "../components/TopNav";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import { Link } from "react-router-dom";
 
 function Admin() {
+  
   return (
     <>
       <Header />
       <TopNav />
-
+      <p className="p3">
+        Admin Dashboard
+      </p>
+      <button className="select_btn">
+        Select AMA!</button>
       <div className="admin_page">
-      <div className="toggle">
-        <Link className="toggle_link" to="/admin">  
-      <button className="toggle_btn">History</button> </Link>
-        <Link className="toggle_link" to="/select">
-      <button className="toggle_btn">Select</button>
-      </Link>
-        </div>
       <div className="ama_history">
       <p className="p1">
         feed with the past AMAs, including name, fun facts, and blurb,  will show here
